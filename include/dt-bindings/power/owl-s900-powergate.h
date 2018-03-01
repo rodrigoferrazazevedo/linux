2499e82ae6bbde90deeea3251f553b9d06425282
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2018-03-01 20:20:30 +0530

    dt-bindings: power: Add Actions Semi S900 SPS
    Define power domains for Actions Semi S900 SoC Smart Power System (SPS).
    
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>

---
+++ include/dt-bindings/power/owl-s900-powergate.h
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Actions Semi S900 SPS
+ *
+ * Copyright (c) 2018 Linaro Ltd.
+ */
+#ifndef DT_BINDINGS_POWER_OWL_S900_POWERGATE_H
+#define DT_BINDINGS_POWER_OWL_S900_POWERGATE_H
+
+#define S900_PD_GPU_B	3
+#define S900_PD_VCE	4
+#define S900_PD_SENSOR	5
+#define S900_PD_VDE	6
+#define S900_PD_HDE	7
+#define S900_PD_USB3	8
+#define S900_PD_DDR0	9
+#define S900_PD_DDR1	10
+#define S900_PD_DE	13
+#define S900_PD_NAND	14
+#define S900_PD_USB2_H0	15
+#define S900_PD_USB2_H1	16
+
+#endif
